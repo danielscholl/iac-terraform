766f7405c8114cef1a2203068ef0edd62bcd770c
Daniel <dascholl@microsoft.com>
2021-12-22 14:30:20 -0600

    Updated

--- modules/ad-application/outputs.tf
+++ modules/ad-application/outputs.tf
@@ -1,25 +1,21 @@
-##############################################################
-# This module allows the creation of a AD Application
-##############################################################
-
 output "name" {
-  value       = azuread_application.main.display_name
+  value       = var.aad_client_id != "" ? null : azuread_application.main[0].display_name
   description = "The display name of the application."
 }
 
 output "id" {
-  value       = azuread_application.main.application_id
+  value       = var.aad_client_id != "" ? null : azuread_application.main[0].application_id
   description = "The ID of the application."
 }
 
 output "object_id" {
-  value       = azuread_application.main.object_id
+  value       = var.aad_client_id != "" ? null : azuread_application.main[0].object_id
   description = "The object ID of the application."
 }
 
 output "roles" {
-  value = {
-    for r in azuread_application.main.app_role :
+  value = var.aad_client_id != "" ? null : {
+    for r in azuread_application.main[0].app_role :
     r.display_name => {
       id          = r.id
       name        = r.display_name
@@ -32,7 +28,7 @@ output "roles" {
 }
 
 output "password" {
-  value       = azuread_application_password.main.0.value
+  value       = var.aad_client_id != "" ? null : azuread_application_password.main.0.value
   sensitive   = true
   description = "The password for the application."
 }
