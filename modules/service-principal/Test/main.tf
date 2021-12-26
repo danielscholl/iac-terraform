8d8433e85584e28fdbf4d5d4d80eb824eae76ea0
Daniel <dascholl@microsoft.com>
2021-12-26 14:46:06 -0600

    Fixed Service Principal

--- modules/service-principal/Test/main.tf
+++ modules/service-principal/Test/main.tf
@@ -16,17 +16,26 @@ module "resource_group" {
 module "service_principal" {
   source = "../"
 
-  name     = format("iac-terraform-%s", module.resource_group.random)
-  role     = "Contributor"
-  scopes   = [module.resource_group.id]
-  end_date = "1W"
+  name            = format("iac-terraform-%s", module.resource_group.random)
+  role            = "Contributor"
+  scopes          = [module.resource_group.id]
+  create_for_rbac = false
+  #end_date = "1W"
 
-  api_permissions = [
-    {
-      name = "Microsoft Graph"
-      app_roles = [
-        "Directory.Read.All"
-      ]
-    }
-  ]
+  #api_permissions = [
+  #  {
+  #    name = "Microsoft Graph"
+  #    app_roles = [
+  #      "Directory.Read.All"
+  #    ]
+  #  }
+  #]
+
+  object_id = "a6737152-2e8c-4db6-996a-d85694845b6f"
+
+  principal = {
+    name     = "iac-terraform-87800d4ad95deb0c"
+    appId    = "9a4cc2ab-63c5-45f7-a05a-002de7105e53"
+    password = "apassword"
+  }
 }
